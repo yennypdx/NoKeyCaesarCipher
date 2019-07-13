fafa4f1fe9aa0c106db36fb449d2d23e47fb19fe
Yang Wang <yang.wang@oit.edu>
2019-07-12 23:03:16 -0700

    Update Program.cs
    added loop option to decrypt

--- NoKeyDecipher/Program.cs
+++ NoKeyDecipher/Program.cs
@@ -2,74 +2,65 @@
 using System.IO;
 using System.Linq;
 
-namespace MonoAlphaDecipher
+namespace NoKeyDecipher
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please type in location of desired file to decrypt: ");
-            //var fPath = Console.ReadLine();
-            var fPath = "C:\\emus\\code.txt";
-            var text = File.ReadLines(fPath);
-
-            var encryptedText = string.Join(",", text.ToArray()).ToLower();
-
-            encryptedText = new String(encryptedText.Where(Char.IsLetter).ToArray());
-
-            Console.WriteLine("Encrypted: " + encryptedText);
-
-            String decryptedText = "";
-
-<<<<<<< HEAD:MonoAlphaDecipher/Program.cs
-=======
-            //var test = 0;
->>>>>>> 540442b9bcc5e5f293abf049484d335a80132948:NoKeyDecipher/Program.cs
-            var decryptedCorrect = false;
-            var key = 0;
-            while (decryptedCorrect == false || key < 26)
+            var end = false;
+            while (!end)
             {
-                foreach (char testChar in encryptedText)
+                Console.WriteLine("Please type in location of desired file to decrypt: ");
+                var fPath = Console.ReadLine();
+                //var fPath = "C:\\emus\\code.txt";
+                var text = File.ReadLines(fPath);
+
+
+                var encryptedText = string.Join(",", text.ToArray()).ToLower();
+
+                encryptedText = new String(encryptedText.Where(Char.IsLetter).ToArray());
+
+                Console.WriteLine("Encrypted: " + encryptedText);
+
+                String decryptedText = "";
+                var decryptedCorrect = false;
+                var key = 0;
+                while (decryptedCorrect == false && key < 26)
                 {
-                    var asciiTestChar = (int)testChar;
-                    asciiTestChar = (asciiTestChar - 97);
-                    asciiTestChar = (asciiTestChar + key) % 26;
-                    asciiTestChar += 97;
-                    var realTestChar = (char)asciiTestChar;
-                    decryptedText += realTestChar.ToString();
+                    foreach (char testChar in encryptedText)
+                    {
+                        var asciiTestChar = (int) testChar;
+                        asciiTestChar = (asciiTestChar - 97);
+                        asciiTestChar = (asciiTestChar + key) % 26;
+                        asciiTestChar += 97;
+                        var realTestChar = (char) asciiTestChar;
+                        decryptedText += realTestChar.ToString();
+                    }
+
+                    if (decryptedText.Contains("the"))
+                    {
+                        decryptedCorrect = true;
+                        Console.WriteLine("Decrypted: " + decryptedText);
+                    }
+
+                    decryptedText = "";
+                    key += 1;
                 }
 
-                if (decryptedText.Contains("the"))
+                if (decryptedCorrect != true)
                 {
-                    decryptedCorrect = true;
-                    Console.WriteLine("Decrypted: " + decryptedText);
+
+                    Console.WriteLine("Key Not Found");
                 }
+                else
+                    Console.WriteLine("Key Found");
 
-                decryptedText = "";
-                key += 1;
+                Console.WriteLine("Would you like to decrypt another file? [y/n]");
+                var read = Console.ReadLine();
+                if (read == "n")
+                    end = true;
             }
-
-            if (decryptedCorrect != true)
-            {
-            
-                Console.WriteLine("Key Not Found");
-            }
-            else
-                Console.WriteLine("Key Found");
-
-            //var mostFreqLetter = encryptedText.GroupBy(x => x).OrderByDescending(x => x.Count()).First().Key;
-            //var secondMostFreqLetter = encryptedText.GroupBy(x => x).OrderByDescending(x => x.Count()).;
-            //Console.WriteLine("Most Freq Used Letter: " + mostFreqLetter);
-            //var asciiValueOfLetter = (int) mostFreqLetter;
-
-
-            //Console.WriteLine("Ascci Letter: " + asciiValueOfLetter);
-
-            //var key = (asciiValueOfLetter + 128) % 128;
-
-            //Console.WriteLine("Key: " + key);
-
-            Console.ReadKey();
         }
     }
 }
